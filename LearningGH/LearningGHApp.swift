845b8b25506238a42a11dff05601b46fda52e678
hansleonp <pawlaczyk99@gmail.com>
2022-01-27 11:42:21 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Hans-Leon Pawlaczyk on 27.01.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
